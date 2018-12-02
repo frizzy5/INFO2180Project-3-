bd7f281419827b0bc3740219f10a3fc0fe2a0eaf
Jevoy <jevoy.charvis@mymona.uwi.edu>
2018-12-02 22:38:50 +0000

    edit2

--- schema.sql
+++ schema.sql
@@ -14,4 +14,24 @@ CREATE TABLE users(
     PRIMARY KEY(id)
 );
 
+CREATE TABLE jobs(
+    id int(10) NOT NULL auto_increment,
+    job_title VARCHAR(250) NOT NULL,
+    job_description LONGTEXT,
+    category VARCHAR(50),
+    company_name VARCHAR(80),
+    company_location VARCHAR(250),
+    date_posted DATE,
+    PRIMARY KEY(id)
+);
 
+CREATE TABLE jobsAppliedFor(
+    id int(10) NOT NULL auto_increment,
+    job_id int(10),
+    user_id int(10),
+    date_applied date,
+    PRIMARY KEY(id),
+    FOREIGN KEY(job_id) REFERENCES jobs(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+INSERT INTO users(firstname,lastname,password,telephone,email,date_joined) VALUES("Jevoy","Charvis","password123","18765899300","admin@hireme.com","2018-4-2");
